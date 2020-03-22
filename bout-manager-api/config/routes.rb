5cf451488ec05265eb1515c98f9ec893e00b390f
Jess Rezac <jess.rezac@gmail.com>
2020-03-22 09:46:42 -0500

    Begin Doorkeeper setup for API

--- bout-manager-api/config/routes.rb
+++ bout-manager-api/config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  use_doorkeeper
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  use_doorkeeper do 
+    skip_contollers :applications, :authorized_applications
+  end
+  
+  namespace :api do
+    namespace :v1 do
+        devise_for :users, controllers: {
+          registrations: 'api/v1/users/registrations'
+        }, skip: [:sessions, :password]
+    end
+  end
 
   root to: "home#index" 
 end
