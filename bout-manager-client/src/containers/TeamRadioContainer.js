6c60eedc622c7ef7aebd76c672dc38b5baab4573
Jess Rezac <jess.rezac@gmail.com>
2020-04-05 22:24:43 -0500

    Add renderTeamRadios function to TeamRadioContainer

--- bout-manager-client/src/containers/TeamRadioContainer.js
+++ bout-manager-client/src/containers/TeamRadioContainer.js
@@ -3,29 +3,34 @@ import TeamRadio from '../components/TeamRadio'
 
 class TeamRadioContainer extends PureComponent {
 
+	renderTeamRadios = () => {
+		return this.props.teams.map(team => {
+			return (
+				<TeamRadio
+					id={team.id}
+					name={
+						team.attributes.organization
+							.name
+					}
+					orgType={
+						team.attributes.organization
+							.org_type
+					}
+				/>
+			)
+		})
+	}
+
     render() {
         return (
 			<section className="section">
 				<div className="container">
-					<h2 className="title is-2">Select A Team</h2>
+					<h2 className="title is-2">Select Your Team</h2>
 					<form action="">
 						{this.props.teams.length > 0
-							? this.props.teams.map(team => {
-									return (
-										<TeamRadio
-											id={team.id}
-											name={
-												team.attributes.organization
-													.name
-											}
-											orgType={
-												team.attributes.organization
-													.org_type
-											}
-										/>
-									)
-							  })
-							: null}
+							? this.renderTeamRadios()
+							: <p className="content">Choose a district to load active teams.</p>}
+						
 					</form>
 				</div>
 			</section>
