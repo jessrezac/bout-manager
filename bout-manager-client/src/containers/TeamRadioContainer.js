39a187a53618346c3b2955fbe558e7a59a85688b
Jess Rezac <jess.rezac@gmail.com>
2020-04-07 21:50:15 -0500

    Create TeamPeople controller in API, begin preparing RoleSelectorForm in Client

--- bout-manager-client/src/containers/TeamRadioContainer.js
+++ bout-manager-client/src/containers/TeamRadioContainer.js
@@ -3,6 +3,10 @@ import TeamRadio from '../components/TeamRadio'
 
 class TeamRadioContainer extends PureComponent {
 
+	state = {
+		selectedTeam: null
+	}
+
 	renderTeamRadios = () => {
 		return this.props.teams.map(team => {
 			return (
@@ -21,16 +25,49 @@ class TeamRadioContainer extends PureComponent {
 		})
 	}
 
+	handleSubmit = e => {
+		e.preventDefault()
+		const configObj = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+				Authorization: "Bearer " + this.props.access_token
+			},
+			body: JSON.stringify({
+				api_v1_team_person: {
+					team_id: this.state.selectedTeam,
+					person_id: this.props.personId
+				}
+			})
+		}
+		fetch(
+			`http://localhost:3000/api/v1/team_people`,
+			configObj
+		)
+			.then(resp => resp.json())
+			.then(data => {
+				this.setState({
+					firstName: data.first_name || "",
+					lastName: data.last_name || "",
+					phone: data.phone || "",
+					email: data.email || "",
+					snapchat: data.snapchat || "",
+					instagram: data.instagram || ""
+				})
+			})
+
+	}
+
     render() {
         return (
 			<section className="section">
 				<div className="container">
 					<h2 className="title is-2">Select Your Team</h2>
-					<form action="">
+					<form onSubmit={this.handleSubmit}>
 						{this.props.teams.length > 0
 							? this.renderTeamRadios()
 							: <p className="content">Choose a district to load active teams.</p>}
-						
 					</form>
 				</div>
 			</section>
