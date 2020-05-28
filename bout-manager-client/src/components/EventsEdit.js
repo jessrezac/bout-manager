0c1bde95467f942b60f6a1a01a848a5f73896bd7
Jess Rezac <jess.rezac@gmail.com>
2020-05-27 22:03:11 -0500

    Use EventsForm component in EventsEdit in Client

--- bout-manager-client/src/components/EventsEdit.js
+++ bout-manager-client/src/components/EventsEdit.js
@@ -1,9 +1,87 @@
 import React, { Component } from "react"
+import { connect } from "react-redux"
+import { setEntities } from "../actions/entities"
+import EventsForm from "./EventsForm"
+
+import normalize from "json-api-normalizer"
 
 class EventsEdit extends Component {
+	state = {
+		name: "",
+		location: "",
+		datetime: null,
+		selectedTeams: [],
+	}
+
+	// TODO: state should be set to actual event details
+
+	handleTeamInput = (selectedTeams) => {
+		this.setState({ selectedTeams })
+	}
+
+	handleChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+		})
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault()
+		const configObj = {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+				Authorization: "Bearer " + this.props.accessToken,
+			},
+			body: JSON.stringify({
+				name: this.state.name,
+				location: this.state.location,
+				datetime: this.state.datetime,
+				teams: this.state.selectedTeams,
+			}),
+		}
+		fetch(`http://localhost:3000/api/v1/events`, configObj)
+			.then((resp) => resp.json())
+			.then((data) => {
+				let normalizedData = normalize(data)
+				this.props.setEntities(normalizedData)
+			})
+	}
+
 	render() {
-		return <div>Editor</div>
+		return (
+			<section className="section">
+				<div className="container">
+					<h1 className="title is-1">Update Event</h1>
+					<EventsForm
+						handleSubmit={this.handleSubmit}
+						handleChange={this.handleChange}
+						handleTeamInput={this.handleTeamInput}
+						{...this.state}
+					/>
+				</div>
+			</section>
+		)
+	}
+
+	componentDidMount() {
+		const event = this.props.events[this.props.match.params.eventId]
+
+		this.setState({
+			name: event.name,
+			location: event.location,
+			datetime: event.datetime,
+			selectedTeams: event.teams,
+		})
 	}
 }
 
-export default EventsEdit
+const mapStateToProps = (state) => {
+	return {
+		accessToken: state.user.user.access_token,
+		events: state.entities.event,
+	}
+}
+
+export default connect(mapStateToProps, { setEntities })(EventsEdit)
